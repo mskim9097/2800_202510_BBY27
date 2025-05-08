03e46d281bc4ad56b822cccd61f75c574978f90b
RayenBMoussa <rayenbenmousa@gmail.com>
2025-05-07 17:01:48 -0700

    update landing and signUp pages design

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("./utils.js");
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
@@ -13,26 +13,26 @@ const PORT = process.env.PORT || 3000;
 //setting ejs as the view engine
 app.set('view engine', 'ejs');
 
-const mongodb_host = process.env.MONGODB_HOST;
-const mongodb_user = process.env.MONGODB_USER;
-const mongodb_password = process.env.MONGODB_PASSWORD;
-const mongodb_database = process.env.MONGODB_DATABASE;
-const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+// const mongodb_host = process.env.MONGODB_HOST;
+// const mongodb_user = process.env.MONGODB_USER;
+// const mongodb_password = process.env.MONGODB_PASSWORD;
+// const mongodb_database = process.env.MONGODB_DATABASE;
+// const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 
-const node_session_secret = process.env.NODE_SESSION_SECRET;
+// const node_session_secret = process.env.NODE_SESSION_SECRET;
 
-var {database} = include('databaseConnection');
+// var {database} = include('databaseConnection');
 
-const userCollection = database.db(mongodb_database).collection('user');
+// const userCollection = database.db(mongodb_database).collection('user');
 
 app.use(express.urlencoded({extended: false}));
 
-var mongoStore = MongoStore.create({
-	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
-	crypto: {
-		secret: mongodb_session_secret
-	}
-})
+// var mongoStore = MongoStore.create({
+// 	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+// 	crypto: {
+// 		secret: mongodb_session_secret
+// 	}
+// })
 
 app.post('/createUser', async (req, res) => {
     var firstName = req.body.firstName;
