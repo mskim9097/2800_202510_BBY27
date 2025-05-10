8d1665c4267cc2515030630049cdef6d8fb60b5c
RayenBMoussa <rayenbenmousa@gmail.com>
2025-05-09 20:39:03 -0700

    test my code in this branch

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,9 +4,11 @@ const Joi = require("joi");
 const MongoStore = require('connect-mongo');
 const session = require('express-session');
 const bcrypt = require("bcrypt");
+const fs = require("fs");
 const saltRounds = 12;
 const expireTime = 24 * 60 * 60 * 1000;
-const app = express();
+const path = require("path");
+
 
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
@@ -15,95 +17,69 @@ const mongodb_database = process.env.MONGODB_DATABASE;
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 
-var {database} = include('databaseConnection');
+var { database } = include('databaseConnection');
 const userCollection = database.db(mongodb_database).collection('user');
 
-app.use(express.urlencoded({extended: false}));
 
-var mongoStore = MongoStore.create({
-	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
-	crypto: {
-		secret: mongodb_session_secret
-	}
-})
-
-app.use(session({ 
-    secret: node_session_secret,
-    store: mongoStore, //default is memory store 
-    saveUninitialized: false, 
-    resave: true
-}
-));
-
-//logic related to common routes like login... goes here
-
-const express = require('express');
-const router  = express.Router();
-const { isAuthenticated } = require('../middleware/auth');
-const { database } = require('../utils/databaseConnection');
-const speciesCollection = database.db('yourDBname').collection('species'); 
-
-// show the “add species” form
-router.get("/add-species", isAuthenticated, (req, res) => {
-  res.render("add-species");
-});
-
-// handle the form POST
-router.post("/add-species", isAuthenticated, async (req, res) => {
+// Handle adding species
+const addSpecies = async (req, res) => {
   const newSpecies = {
-    commonName:     req.body.commonName,
+    commonName: req.body.commonName,
     scientificName: req.body.scientificName,
-    image:          req.body.image,
-    description:    req.body.description,
+    image: req.body.image,
+    description: req.body.description,
     location: {
       center: {
         lat: parseFloat(req.body.lat),
-        lng: parseFloat(req.body.lng)
+        lng: parseFloat(req.body.lng),
       },
-      rangeKm: parseFloat(req.body.rangeKm)
-    }
+      rangeKm: parseFloat(req.body.rangeKm),
+    },
   };
-  await speciesCollection.insertOne(newSpecies);
+
+  //  Skip database for now
+  // await speciesCollection.insertOne(newSpecies);
+
+  // Save to dummy_species.json
+  const file = path.join(__dirname, "../dummy_species.json");
+  const speciesList = JSON.parse(fs.readFileSync(file, "utf8"));
+  speciesList.push(newSpecies);
+  fs.writeFileSync(file, JSON.stringify(speciesList, null, 2));
+
   res.redirect("/species");
-});
+};
 
-// list all species
-router.get("/species", isAuthenticated, async (req, res) => {
-  const speciesList = await speciesCollection.find().toArray();
-  res.render("pages/species", { species: speciesList });
-});
 
-module.exports = router;
 
 // sign up function.
 const createUser = async (req, res) => {
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-    var email = req.body.email;
-    var password = req.body.password;
-    var type = req.body.type;
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+  var password = req.body.password;
+  var type = req.body.type;
 
-    const schema = Joi.object({
-        firstName: Joi.string().alphanum().max(20).required(),
-        lastName: Joi.string().alphanum().max(20).required(),
-        email: Joi.string().email().max(30).required(),
-        password: Joi.string().max(20).required()
-    });
-    const validationResult = schema.validate({firstName, lastName, email, password});
-    /*
-    if (validationResult.error != null) {
-        res.send(`
-            Invalid email/password combination.<br><br>
-            <a href="/signup">Try again</a>
-            `);
-        return;
-    }*/
-    var hashedPassword = await bcrypt.hash(password, saltRounds);
+  const schema = Joi.object({
+    firstName: Joi.string().alphanum().max(20).required(),
+    lastName: Joi.string().alphanum().max(20).required(),
+    email: Joi.string().email().max(30).required(),
+    password: Joi.string().max(20).required()
+  });
+  const validationResult = schema.validate({ firstName, lastName, email, password });
+  /*
+  if (validationResult.error != null) {
+      res.send(`
+          Invalid email/password combination.<br><br>
+          <a href="/signup">Try again</a>
+          `);
+      return;
+  }*/
+  var hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    await userCollection.insertOne({firstName: firstName, lastName: lastName, email: email, password: hashedPassword, type: type});
+  await userCollection.insertOne({ firstName: firstName, lastName: lastName, email: email, password: hashedPassword, type: type });
 
-    res.redirect("/");
+  res.redirect("/");
 }
 
 
-module.exports = {createUser};
+module.exports = { createUser , addSpecies };
--- routes/researcherRoutes.js
+++ routes/researcherRoutes.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const {createQuest} = require('../controllers/researcherController');
-
+const { addSpecies } = require("../controllers/userController");
 router.get('/dashboard', (req, res) => {
     // This will render views/pages/researcherDashboard.ejs
     res.render('pages/researcherDashboard2'); 
@@ -11,7 +11,15 @@ router.get('/dashboard', (req, res) => {
 
 router.get('/testQuest', (req, res) => {
     res.render('pages/testQuest');
-})
+});
+
 router.get('/createQuest', createQuest);
+
+router.get("/add-species",(req, res) => {
+    res.render('pages/species-add');
+})
+
+// show the “add species” form
+router.post("/add-species", addSpecies);
 
 module.exports = router;
