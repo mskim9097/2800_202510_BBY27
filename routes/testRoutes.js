76217c037caa6c6e9db8d38abc8b3ccab5ae108c
mskim9097 <mskim9097@gmail.com>
2025-05-14 02:02:20 -0700

    implement test function to create and select image

---
+++ routes/testRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const {testCreateImage, testSelectImage} = require('../controllers/testController.js');
+const fs = require('fs');
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+        const uniqueName = Date.now() + path.extname(file.originalname);
+        cb(null, uniqueName);
+    }
+});
+const upload = multer({ storage: storage });
+
+router.get('/testCreateImage', (req, res) => {
+    res.render('pages/testCreateImage');
+});
+router.post('/testCreateImage', upload.single('image'), testCreateImage);
+
+router.get('/testSelectImage', testSelectImage, (req, res) => {
+    res.render('pages/testSelectImage');
+});
+
+module.exports = router;
