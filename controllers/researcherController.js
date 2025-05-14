80d058aa00918e2dc1de7c50d42fdf13ff7f5842
Blair Tate <btate7@my.bcit.ca>
2025-05-14 14:28:06 -0700

    Added middleware for creating a species, updating a species, and getting a species

--- controllers/researcherController.js
+++ controllers/researcherController.js
@@ -15,10 +15,12 @@ const mongodb_database = process.env.MONGODB_DATABASE;
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 
-var {database} = include('databaseConnection');
+var { database } = include('databaseConnection');
 const questCollection = database.db(mongodb_database).collection('quest');
+const speciesCollection = database.db(mongodb_database).collection('spicies');
 
-app.use(express.urlencoded({extended: false}));
+
+app.use(express.urlencoded({ extended: false }));
 //logic related to researcher routes goes here
 
 const createQuest = async (req, res) => {
@@ -41,4 +43,62 @@ const createQuest = async (req, res) => {
     res.redirect("/");
 }
 
-module.exports = {createQuest};
+const createSpecies = async (req, res) => {
+    const speciesName = req.query.speciesName;
+    const speciesInfo = req.query.speciesInfo;
+    const speciesScientificName = req.query.speciesScientificName;
+    const speciesImage = [];
+    speciesImage[0] = req.query.speciesImage;
+
+    //This fields are set to null until updated by quests
+    const speciesQuests = [];
+    const speciesLocations = {};
+
+    await speciesCollection.insertOne({
+        speciesName: speciesName,
+        speciesInfo: speciesInfo,
+        speciesImage: speciesImage,
+        speciesLocation: speciesLocations,
+        speciesQuantity: speciesQuantity,
+        speciesQuests: speciesQuests,
+        speciesScientificName: speciesScientificName
+    });
+    res.redirect("/");
+}
+
+//This updates the same fields as createSpecies
+const updateSpecies = async (req, res) => {
+    const speciesName = req.query.speciesName;
+    const speciesScientificName = req.query.speciesScientificName;
+    const speciesInfo = req.query.speciesInfo;
+    const speciesImage = [];
+    speciesImage[0] = req.query.speciesImage;
+    //list of Quest IDs related to this species
+
+    await speciesCollection.updateOne(
+        { speciesName: speciesName },
+        {
+            $set: {
+                speciesInfo: speciesInfo,
+                speciesImage: speciesImage,
+                speciesLocation: speciesLocation,
+                speciesQuantity: speciesQuantity,
+                speciesQuests: speciesQuests,
+                speciesScientificName: speciesScientificName
+            }
+        }
+    );
+    res.redirect("/");
+}
+
+const getSpecies = async (req, res) => {
+    const speciesScientificName = req.query.speciesScientificName;
+    const species = await speciesCollection.findOne({ speciesName: speciesScientificName });
+    if (species) {
+        res.status(200).json(species);
+    } else {
+        res.status(404).json({ message: "Species not found" });
+    }
+}
+
+module.exports = { createQuest, createSpecies, updateSpecies, getSpecies };
