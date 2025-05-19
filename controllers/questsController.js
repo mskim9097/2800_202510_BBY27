140ba520509ec1e759e413e5f16333f0402f465e
mskim9097 <mskim9097@gmail.com>
2025-05-19 07:24:15 -0700

    edit createQuest using new model

--- controllers/questsController.js
+++ controllers/questsController.js
@@ -4,7 +4,7 @@ const Joi = require("joi");
 const Quest = require("../models/questModel");
 
 const appClient = require('../databaseConnection').database;
-const questCollection = appClient.db('biodiversityGo').collection('quest');
+const questCollection = appClient.db('biodiversityGo').collection('quests');
 const speciesCollection = appClient.db('biodiversityGo').collection('species');
 
 // searchTarget function to search spicies that match input from mongoDB
@@ -18,70 +18,37 @@ const searchTarget = async (req, res) => {
         .toArray();
     res.json(results);
 };
-/*
-const createQuest = async (req, res) => {
-    try {
-        const {
-            title,
-            mission,
-            latitude,
-            longitude,
-            target,      // this is the species ID from the dropdown
-            timeOfDay,
-            difficulty
-        } = req.body;
-        const userId = req.session.userId;
-        console.log('BODY:', req.body);
-        console.log('SESSION:', req.session);
 
-        const newQuest = new Quest({
-            title,
-            mission,
-            location: {
+// createQuest function that saves quest document in mongoDB.
+const createQuest = async (req, res, next) => {
+    var questTitle = req.body.title;
+    var questMission = req.body.mission;
+    var latitude = req.body.latitude;
+    var longitude = req.body.longitude;
+    var target = req.body.target;
+    var questTimeOfDay = req.body.timeOfDay;
+    var questDifficulty = req.body.difficulty;
+
+    const species = await speciesCollection.findOne({ speciesName: target });
+    const userId = req.session.userId;
+
+    const newQuest = new Quest({
+            questTitle,
+            questMission,
+            questLocation: {
                 type: 'Point',
                 coordinates: [parseFloat(longitude), parseFloat(latitude)]
             },
-            targetSpecies: target,  // Store the species ID
-            timeOfDay,
-            difficulty,
-            createdBy: userId,  // assuming req.user is populated by auth middleware
-            acceptedBy: []            // starts empty
+            speciesId: species._id,  // Store the species ID
+            questTimeOfDay,
+            questDifficulty,
+            questCreatedBy: userId,  // assuming req.user is populated by auth middleware
+            questAcceptedBy: [],            // starts empty
+            questFieldNote: '',
+            questImage: ''
         });
+    await newQuest.save();
 
-        await newQuest.save();
-        res.redirect("/user/researcher");
-    } catch (error) {
-        console.error('Error creating quest:', error);
-        res.status(500).send('Something went wrong while creating the quest.');
-    }
-};
-
-*/
-// createQuest function that saves quest document in mongoDB.
-const createQuest = async (req, res, next) => {
-    var title = req.body.title;
-    var latitude = req.body.latitude;
-    var longitude = req.body.longitude;
-    var mission = req.body.mission;
-    var target = req.body.target;
-    var timeOfDay = req.body.timeOfDay;
-    var difficulty = req.body.difficulty;
-
-    const species = await speciesCollection.findOne({ speciesName: target });
-
-    await questCollection.insertOne({
-        questTitle: title,
-        questLatitude: latitude,
-        questLongitude: longitude,
-        questMission: mission,
-        speciesId: species._id,
-        questTimeOfDay: timeOfDay,
-        questDifficulty: difficulty,
-        questCreatedBy: req.session.user,
-        questImage: null,
-        questFieldNote: null,
-        questAcceptedBy: null
-    });
     next();
 }
 
--- models/questModel.js
+++ models/questModel.js
@@ -3,17 +3,19 @@
 const mongoose = require('mongoose');
 
 const questSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    mission: { type: String, required: true },
-    location: {
+    questTitle: { type: String, required: true },
+    questMission: { type: String, required: true },
+    questLocation: {
         type: { type: String, enum: ['Point'], required: true },
         coordinates: { type: [Number], required: true }
     },
-    targetSpecies: { type: mongoose.Schema.Types.ObjectId, ref: 'Species' },//required: true
-    timeOfDay: { type: String, enum: ['Morning', 'Afternoon', 'Evening', 'Night'], required: true },
-    difficulty: { type: String, enum: ['Easy', 'Medium', 'Hard'], required: true },
-    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },//required: true
-    acceptedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+    speciesId: { type: mongoose.Schema.Types.ObjectId, ref: 'Species' },//required: true
+    questTimeOfDay: { type: String, enum: ['Morning', 'Afternoon', 'Evening', 'Night'], required: true },
+    questDifficulty: { type: String, enum: ['Easy', 'Medium', 'Hard'], required: true },
+    questCreatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },//required: true
+    questAcceptedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    questFieldNote: {type: String},
+    questImage: {type: String}
 }, {
     timestamps: true
 });
